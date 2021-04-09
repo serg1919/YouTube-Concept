545c9d4b4c77a6abbeb1b060edc67f8a2c17ec4d
serg1919 <50267566+serg1919@users.noreply.github.com>
2021-04-09 20:17:52 +0300

    add sliders

--- js/slick.js
+++ js/slick.js
@@ -1,6 +1,30 @@
-$(document).ready(function(){
+$(function(){
   $('.channel__slider').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
+
+    prevArrow: '<button type="button" class="arrow prev"></button>',
+    nextArrow: '<button type="button" class="arrow next"></button>'
   });
+
+
+  $('.r__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+
+    prevArrow: '<button type="button" class="r-arrow r-prev"></button>',
+    nextArrow: '<button type="button" class="r-arrow r-next"></button>'
+  });
+
+
+  $('.food__slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    infinite: true,
+
+    prevArrow: '<button type="button" class="f-arrow f-prev"></button>',
+    nextArrow: '<button type="button" class="f-arrow f-next"></button>'
+  });
+
 });
