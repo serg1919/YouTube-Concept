d9b4e2b4b5a26b5c4c642786422d93eff16b89fa
serg1919 <50267566+serg1919@users.noreply.github.com>
2021-04-13 10:38:04 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -2,9 +2,46 @@ $(function(){
   $('.channel__slider').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
+    arrows: true,
 
     prevArrow: '<button type="button" class="arrow prev"></button>',
-    nextArrow: '<button type="button" class="arrow next"></button>'
+    nextArrow: '<button type="button" class="arrow next"></button>',
+
+      responsive: [
+      {
+        breakpoint: 1801,
+        settings: {
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 1501,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1151,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 2,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 501,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      }
+    ]
+
   });
 
 
@@ -14,7 +51,31 @@ $(function(){
     infinite: true,
 
     prevArrow: '<button type="button" class="r-arrow r-prev"></button>',
-    nextArrow: '<button type="button" class="r-arrow r-next"></button>'
+    nextArrow: '<button type="button" class="r-arrow r-next"></button>',
+
+    responsive: [
+      {
+        breakpoint: 1501,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 1151,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      },
+
+    ]
+
   });
 
 
@@ -24,7 +85,43 @@ $(function(){
     infinite: true,
 
     prevArrow: '<button type="button" class="f-arrow f-prev"></button>',
-    nextArrow: '<button type="button" class="f-arrow f-next"></button>'
+    nextArrow: '<button type="button" class="f-arrow f-next"></button>',
+
+    responsive: [
+      {
+        breakpoint: 1801,
+        settings: {
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 1501,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 1151,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 2,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 501,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      }
+    ]
+
   });
 
 });
@@ -39,5 +136,3 @@ searchBtn.addEventListener('click', (e) => {
   logo.classList.toggle('logo--active');
 
 });
-
-// console.log(e);
